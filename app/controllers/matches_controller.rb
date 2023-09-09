9b6ba0f28801944006c5bfe3899f44bf5be39e34
Derek Neighbors <dneighbo@gmail.com>
2023-09-08 20:11:29 -0700

    add weight class to form add weight class & fighter to create so if fails and rerender no issues fix spacing on disable

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -34,6 +34,8 @@ class MatchesController < ApplicationController
     params[:match][:status_id] = params[:match][:status_id].to_i if params[:match][:status_id].present?
     puts params[:match][:status_id]
     @match = Match.new(match_params)
+    @weight_classes = WeightClass.all
+    @fighters = Fighter.all
 
     respond_to do |format|
       if @match.save
