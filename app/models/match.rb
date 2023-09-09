c07ddacd78f5e4890119fe6b1d0fe7ebd84da58b
Derek Neighbors <dneighbo@gmail.com>
2023-09-09 06:09:49 -0700

    fix weightclasss/match relationship so doesnt blow up on save

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,7 +2,7 @@ class Match < ApplicationRecord
   belongs_to :fighter_1, class_name: 'Fighter'
   belongs_to :fighter_2, class_name: 'Fighter'
   belongs_to :winner, class_name: 'Fighter', optional: true # Winner is optional because a match might not be finished yet
-  belongs_to :weight_class
+  belongs_to :weight_class, :optional => true
   has_many :rounds
 
   enum status_id: { pending: 0, completed: 1 }
