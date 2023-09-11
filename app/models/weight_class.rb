37cc1a833104b752ef295957c02a426111c43d21
Derek Neighbors <dneighbo@gmail.com>
2023-09-10 17:37:04 -0700

    dial in composite score a bit

--- app/models/weight_class.rb
+++ app/models/weight_class.rb
@@ -17,19 +17,32 @@ class WeightClass < ApplicationRecord
     # Find all fighters in this weight class
     fighters = self.fighters
 
+    # Define weights for each factor
+    winning_percentage_weight = 0.9
+    number_of_fights_weight = 0.05
+    highest_rank_weight = 0.2
+
     # Calculate a composite score for each fighter
     sorted_fighters = fighters.sort_by do |fighter|
       # Winning percentage as the first factor (higher is better)
-      winning_percentage = -fighter.winning_percentage
+      winning_percentage = fighter.winning_percentage * winning_percentage_weight
 
       # Number of fights as the second factor (higher is better)
-      number_of_fights = -(fighter.matches_as_fighter_1.count + fighter.matches_as_fighter_2.count)
+      number_of_fights = (fighter.matches_as_fighter_1.count + fighter.matches_as_fighter_2.count) * number_of_fights_weight
 
       # Highest previous rank as the third factor (lower is better)
-      highest_rank = fighter.highest_rank || Float::INFINITY
+      highest_rank = fighter.highest_rank || Float::INFINITY * highest_rank_weight
 
-      [winning_percentage, number_of_fights, highest_rank]
+      # Calculate the composite score
+      composite_score = winning_percentage + number_of_fights # + highest_rank
+
+      # Sort in descending order of composite score
+      -composite_score
     end
+    # Print out the sorted fighters for debugging
+    # sorted_fighters.each_with_index do |fighter, index|
+    #   puts "#{index + 1} #{fighter.name} Record:#{fighter.wins}-#{fighter.losses}-#{fighter.draws} Winning PCT:#{fighter.winning_percentage} Highest Rank:#{fighter.highest_rank}"
+    # end
 
     # Update the ranks in the WeightClassRank model
     sorted_fighters.each_with_index do |fighter, index|
