e574205c1cd57ee22fcbfc2f2615611881ad2ddc
Derek Neighbors <dneighbo@gmail.com>
2023-10-22 09:17:19 -0700

    visually display powerscore strength

--- app/helpers/fighters_helper.rb
+++ app/helpers/fighters_helper.rb
@@ -1,2 +1,18 @@
 module FightersHelper
+  def score_color(score)
+    case score
+    when 0..45
+      "text-red-500"
+    when 46..50
+      "text-orange-400"
+    when 51..60
+      "text-gray-500"
+    when 61..70
+      "text-purple-500"
+    when 71..80
+      "text-green-500 font-semibold"
+    else
+      "text-green-500 font-bold underline"
+    end
+  end
 end
